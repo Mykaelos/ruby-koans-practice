8e65f4987471533f53cd490843d9d52e917b8edc
Mykaelos <mmaderii@gmail.com>
2018-11-11 14:52:12 -0600

    Finished the next three lessons.

--- triangle.rb
+++ triangle.rb
@@ -15,6 +15,18 @@
 #
 def triangle(a, b, c)
   # WRITE THIS CODE
+  return :equilateral if a == b && b == c
+  return :isosceles if  a == b || b == c || c == a
+  return :scalene
+
+  # Alternate method provided by Cad
+  # return :equilateral if [a,b,c].uniq.length == 1
+  # return :isosceles if [a,b,c].uniq.length == 2
+  # return :scalene
+
+  # MYKAELOS: I prefer the more explicit equal checks above, instead of using
+  # [].uniq, but uniq is much better when you start getting a lot of objects to
+  # check, like 4+ of them.
 end
 
 # Error class used in part 2.  No need to change this code.
