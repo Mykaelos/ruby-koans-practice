7c1571d84226e6adec98816ab2367715fba34bb7
Mykaelos <mmaderii@gmail.com>
2018-11-11 16:59:03 -0600

    Four more lessons...

--- triangle.rb
+++ triangle.rb
@@ -15,6 +15,9 @@
 #
 def triangle(a, b, c)
   # WRITE THIS CODE
+  raise TriangleError, "Triangle sides cannot be zero or less in length." unless [a, b, c].select{ |x| x <= 0 }.length == 0
+  raise TriangleError, "No triangle side can be greater than or equal to the sum of the other two sides." if a + b <= c || b + c <= a || c + a <= b
+
   return :equilateral if a == b && b == c
   return :isosceles if  a == b || b == c || c == a
   return :scalene
@@ -26,9 +29,14 @@ def triangle(a, b, c)
 
   # MYKAELOS: I prefer the more explicit equal checks above, instead of using
   # [].uniq, but uniq is much better when you start getting a lot of objects to
-  # check, like 4+ of them.
+  # check, like 4+ of them. 3 objects is the line where I could go either way.
 end
 
 # Error class used in part 2.  No need to change this code.
 class TriangleError < StandardError
 end
+
+# assert_raise(TriangleError) do triangle(0, 0, 0) end
+# assert_raise(TriangleError) do triangle(3, 4, -5) end
+# assert_raise(TriangleError) do triangle(1, 1, 3) end
+# assert_raise(TriangleError) do triangle(2, 4, 2) end
